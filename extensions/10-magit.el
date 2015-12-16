1053881586cf1456e84056b33c4b7657cc7a0916
roronya <roronya628@gmail.com>
2015-12-16 09:57:28 +0900

    update

--- extensions/10-magit.el
+++ extensions/10-magit.el
@@ -1,5 +1,8 @@
 (global-unset-key "\C-x\C-v")
 (use-package magit
+  :init
+  (add-hook 'magit-mode
+            (lambda ()
+              (local-unset-key "\C-w")))  
   :bind
-  (("C-x C-v C-s" . magit-status))
-  )
+  (("C-x C-v C-s" . magit-status)))

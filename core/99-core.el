e1abcd16257d4724cb10b793ea5647e1f7edfa39
roronya <roronya628@gmail.com>
2015-12-16 10:23:17 +0900

    update

--- core/99-core.el
+++ core/99-core.el
@@ -125,6 +125,7 @@
 (bind-key* "C-x C-h C-f" 'describe-function)
 (bind-key* "C-x C-h C-v" 'describe-variable)
 (bind-key* "C-x C-h C-k" 'describe-key)
+(bind-key* "C-x C-h C-m" 'describe-mode)
 
 ;;; patoronage
 (bind-key* "C-x C-p C-e" 'ein:notebooklist-open)
--- extensions/10-magit.el
+++ extensions/10-magit.el
@@ -1,8 +1,11 @@
 (global-unset-key "\C-x\C-v")
 (use-package magit
   :init
-  (add-hook 'magit-mode
+  (add-hook 'magit-status-mode-hook
             (lambda ()
-              (local-unset-key "\C-w")))  
+              (local-unset-key "\C-w")))
+  (add-hook 'magit-diff-mode-hook
+            (lambda ()
+              (local-unset-key "\C-w")))
   :bind
   (("C-x C-v C-s" . magit-status)))

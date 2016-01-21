66fe5b3fe21d619b578904bd99578b960496e4c8
roronya <yukikanai0204@gmail.com>
2016-01-21 12:25:43 +0900

    [modify] elscreen の挙動を変えた 参照(http://d.hatena.ne.jp/asudofu/20091121/1258778536)

--- core/90-tab.el
+++ core/90-tab.el
@@ -1,5 +1,4 @@
 (require 'elscreen)
-
 (elscreen-start)
 (global-unset-key "\M-t")
 (smartrep-define-key global-map "M-t"
@@ -10,3 +9,40 @@
     ("c" . 'elscreen-clone)
     )
   )
+
+(defun elscreen-insert-internal (screen)
+  (elscreen-clone screen)
+  (elscreen-kill-internal screen))
+
+(defun elscreen-get-gap-next ()
+  (let ((screen-list (sort (elscreen-get-screen-list) '<))
+    (screen 0))
+    (while (eq (nth screen screen-list) screen)
+      (setq screen (+ screen 1)))
+    (nth screen screen-list)))
+
+(defun elscreen-get-packed-num ()
+  (let ((screen-list (sort (elscreen-get-screen-list) '<))
+    (current-screen (elscreen-get-current-screen))
+    (screen 0))
+    (while (not (eq (nth screen screen-list) current-screen))
+      (setq screen (+ screen 1)))
+    screen))
+
+(defun elscreen-pack-list ()
+  (interactive)
+  (let ((next (elscreen-get-gap-next))
+    (pack (elscreen-get-packed-num)))
+    (while next
+      (elscreen-insert-internal next)
+      (setq next (elscreen-get-gap-next)))
+    (elscreen-goto pack)
+    (elscreen-notify-screen-modification 'force)))
+
+;killしたらpackする
+(add-hook 'elscreen-kill-hook 'elscreen-pack-list)
+
+;swap前後で同じバッファを表示
+(defadvice elscreen-swap
+  (after elscreen-swap-jump activate)
+  (elscreen-goto (elscreen-get-previous-screen)))
--- extensions/20-web-mode.el
+++ extensions/20-web-mode.el
@@ -5,4 +5,6 @@
          )
   :config
   (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
   )

9eb60f28fd76ff7190d2b6616056eb7942a8bb33
roronya <yukikanai0204@gmail.com>
2016-01-21 11:56:48 +0900

    [modify] web-mode の html のインデント幅を変えた

--- extensions/20-web-mode.el
+++ extensions/20-web-mode.el
@@ -1,12 +1,8 @@
 (use-package web-mode
   :mode (("\\.php?\\'" . web-mode)
-	 ("\\.html?\\'" . web-mode)
-	 ("\\.twig?\\'" . web-mode)
-	 )
+         ("\\.html?\\'" . web-mode)
+         ("\\.twig?\\'" . web-mode)
+         )
   :config
-  (setq web-mode-html-offset 4)
-  (setq web-mode-css-offset 4)
-  (setq web-mode-script-offset 4)
-  (setq web-mode-php-offset 4)
-  (setq tab-width 4)
+  (setq web-mode-markup-indent-offset 2)
   )

fc4b41ef70ef122a120b061f4e42fbcfd613eb37
roronya <roronya628@gmail.com>
2016-02-08 10:34:00 +0900

    [modify] package の設定加えた

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 (require 'package)
 (package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 (exec-path-from-shell-initialize)
 
